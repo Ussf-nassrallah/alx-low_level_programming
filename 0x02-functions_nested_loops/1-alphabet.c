a7459def74e62fa27c482a9f665c0ca1be6cbd5d
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-03-21 05:42:04 -0700

    task 1 done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function that prints the alphabet, in lowercase followed by new line
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
+}
