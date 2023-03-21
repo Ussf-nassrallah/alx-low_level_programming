7ae0da268bb94490109c63311e34326efe7df3ee
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-03-21 11:47:55 -0700

    task 10 done

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Adds two values
+ * @num1: the first number to be computed
+ * @num2: the second number to be computed
+ * Return: Integer value
+ */
+
+int add(int num1, int num2)
+{
+	int sum = num1 + num2;
+
+	return (sum);
+}
