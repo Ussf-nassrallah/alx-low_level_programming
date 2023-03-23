7aa602b222c41df58bfe21c44e30981010b098f6
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-03-23 13:36:47 -0700

    task 1 done

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - function that checks for a digit
+ * @c: chaeackter will be checked
+ * Return: 0 or 1
+ */
+
+int _isdigit(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
