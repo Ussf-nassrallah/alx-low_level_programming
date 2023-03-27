1f31bd22f74572d3a1e66e33f24cc14f551773ec
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-03-27 16:21:07 -0700

    task 4 done

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_rev - function thats prints a string in reverse
+ * @s: represent the string will be reversed
+ * Return: ...
+ */
+
+void print_rev(char *s)
+{
+	int strsize = strlen(s);
+
+	int i;
+
+	for (i = strsize; i >= 0; i--)
+	{
+		putchar(s[i]);
+	}
+
+	putchar('\n');
+}
