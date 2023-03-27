15d83ce5c16fbd822274ac6305d4d6220030b161
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-03-27 13:21:27 -0700

    task 0 done

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that updates the value it points to 98
+ * @n: number will be updated
+ * Return: ...
+ */
+
+void reset_to_98(int *n)
+{
+	int *ip = n;
+
+	*ip = 98;
+}
