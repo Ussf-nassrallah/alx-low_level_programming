ee8ccba778015b1fac5ce787fab1f9aa4403b0ad
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-03-27 15:50:11 -0700

    task 3 done

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - function that prints a string followed by a new line
+ * @str: represent the string will be printed
+ * Return: ...
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
