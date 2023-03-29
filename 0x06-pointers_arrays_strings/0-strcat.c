cc395f31850b8190941732b17f398e56d966f43d
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-03-29 13:43:02 -0700

    task 0 done

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strcat - function that concatenates two strings
+ * @dest: represent destination
+ * @src: represent content
+ * Return: pointer
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	return strcat(dest, src);
+}
