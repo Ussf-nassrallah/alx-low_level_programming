ff3ed7915c51bbe21ed6ec42d19429fab5b6efdf
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-04-08 13:47:33 -0700

    task 4 done

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,63 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int _isdigit(char *c);
+
+/**
+ * main - entry point
+ * @argc: count of arguments
+ * @argv: arguments victor represent an array of arguments
+ * Return: 0 success
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		int sum;
+
+		int index;
+
+		for (index = 1; index < argc; index++)
+		{
+			if (_isdigit(argv[index]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+			{
+				sum = sum + atoi(argv[index]);
+			}
+		}
+
+		printf("%d\n", sum);
+	}
+
+	return (0);
+}
+
+/**
+ * _isdigit - function checks if character is a number
+ * @c: char will be checked
+ * Return: ...
+ */
+
+int _isdigit(char *c)
+{
+	if (*c >= '0' && *c <= '9')
+	{
+		c++;
+	}
+	else
+	{
+		return (1);
+	}
+
+	return (0);
+}
