0ec864d4ecd97f700d99263a7162fb20bb43c3e8
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-04-08 07:34:35 -0700

    task 1 done

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * @argc: arguments count
+ * @argv: arguments victor
+ * Return: 0 Always success
+*/
+
+int main(int argc, char **argv)
+{
+	/**
+	 * (void) argv - pay no attention to argv.
+	*/
+	(void) argv;
+
+	printf("%d\n", argc);
+
+	return (0);
+}
