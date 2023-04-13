06b1aa488bf9edd0d7b1aad292d862db7202bb94
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-04-13 14:22:19 -0700

    task 2 done

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - function that allocates memory for an array
+ * @nmemb: n nember elements
+ * @size: size of elements
+ * Return: ...
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *output;
+
+	unsigned int index;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (0);
+	}
+
+	output = malloc(nmemb * size);
+
+	if (output == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (index = 0; index < size; index++)
+		{
+			*(output + index) = 0;
+		}
+
+		return (output);
+	}
+}
