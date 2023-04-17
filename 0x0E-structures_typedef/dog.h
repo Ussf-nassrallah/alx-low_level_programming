8f9ab108156caf3add5e2cb54a50b17b9e73d529
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-04-17 14:43:27 -0700

    task 0 done

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+
+#define DOG_H
+
+/**
+ * struct dog - new type for dog properties
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
