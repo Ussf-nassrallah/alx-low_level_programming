3c9f35bb8061a56a8e3e7323ce592170f4f6909d
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-04-19 14:38:01 -0700

    task 3 done

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,45 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: program arguments (arguments count)
+ * @argv: array of arguments
+ * Return: ...
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+
+	char *operation;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	a = atoi(argv[1]);
+
+	b = atoi(argv[3]);
+
+	operation = argv[2];
+
+	if (get_op_func(operation)== NULL || operation[1] != '\0')
+	{
+		printf("Error\n");
+		exit(101);
+	}
+
+	if ((*operation == '/' && b == 0) || (*operation == '%' && b == 0))
+	{
+		printf("Error\n");
+		exit(102);
+	}
+
+	printf("%d\n", get_op_func(operation)(a, b));
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,75 @@
+#include "3-calc.h"
+#include <stdio.h>
+
+/**
+ * op_add - function that returns sum of a and b
+ * @a: first number will be operated
+ * @b: second number will be operated
+ * Return: (a + b)
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - function that returns diff of a and b
+ * @a: first number will be operated
+ * @b: second number will be operated
+ * Return: (a - b)
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - f that return product of a and b
+ * @a: first number will be operated
+ * @b: second number will be operated
+ * Return: (a * b)
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - function that return division of a by b
+ * @a: first number
+ * @b: second number
+ * Return: (a / b)
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - return the remainder of the division of a by b
+ * @a: first number input
+ * @b: second number input
+ * Return: (a % b)
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
