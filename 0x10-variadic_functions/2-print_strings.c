dbb69a52008be0bf1318cde692382f82e9bb33a0
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-04-20 15:05:59 -0700

    task 2 done

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - function that prints strings, followed by a new line
+ * @separator: ele will be printed betwen arguments
+ * @n: arguments count
+ * @...: arguments
+ * Return: Nothing
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+
+	va_list sp;
+
+	va_start(sp, n);
+
+	for (; i < n; i++)
+	{
+		printf("%s", va_arg(sp, char*));
+
+		if (i != (n - 1) && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+
+	va_end(sp);
+
+	printf("\n");
+}
