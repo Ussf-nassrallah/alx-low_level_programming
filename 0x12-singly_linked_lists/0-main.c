f7840fb86ab294471540214a20430889684b31ee
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-04-28 02:34:59 -0700

    prints all the elements of a linked list

--- 0x12-singly_linked_lists/0-main.c
+++ 0x12-singly_linked_lists/0-main.c
@@ -29,12 +29,6 @@ int main(void)
     n = print_list(head);
     printf("-> %lu elements\n", n);
 
-    printf("\n");
-    free(new->str);
-    new->str = NULL;
-    n = print_list(head);
-    printf("-> %lu elements\n", n);
-
     free(new);
     return (0);
 }
