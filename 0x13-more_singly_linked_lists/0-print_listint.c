2d4f1abb229440ba13bef40ecf16f2173d18521d
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-04-29 07:06:33 -0700

    func that prints all the elements of a listint_t list.

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint -  function that prints all the elements of a listint_t list.
+ * @h: head - The entry point into a linked list
+ * Return: int
+*/
+
+size_t print_listint(const listint_t *h)
+{
+	size_t counter = 0;
+
+	while (h)
+	{
+		h = h->next;
+		counter++;
+	}
+
+	return (counter);
+}
