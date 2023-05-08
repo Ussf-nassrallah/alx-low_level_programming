2fee263bcf0a6c6548bf6791e5ef57631fe76cf7
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-05-08 07:47:18 -0700

    function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,9 +9,9 @@
 
 int create_file(const char *fn, char *s)
 {
+	int index = 0;
 	/* Value return from open */
 	int open_output;
-
 	/* value return from write */
 	int write_output;
 
@@ -19,14 +19,19 @@ int create_file(const char *fn, char *s)
 		return (-1);
 
 	open_output = open(fn, O_RDWR | O_CREAT, 0600);
+	write_output = write(open_output, s, strlen(s));
 
 	if (s != NULL)
-		write_output = write(open_output, s, strlen(s));
+	{
+		while (s[index] != '\0')
+		{
+			index++;
+		}
+	}
 
 	if (open_output == -1 || write_output == -1)
 		return (-1);
 
 	close(open_output);
-
 	return (1);
 }
