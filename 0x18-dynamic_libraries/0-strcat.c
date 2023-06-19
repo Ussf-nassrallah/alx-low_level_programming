32206c59c2c109a39635d00c2279d1fbb78e225c
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-06-19 11:42:44 +0100

    Create the dynamic library libdynamic.so

---
+++ 0x18-dynamic_libraries/0-strcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strcat - function that concatenates two strings
+ * @dest: represent destination
+ * @src: represent content
+ * Return: pointer
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	};
+
+	j = 0;
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	};
+
+	return (dest);
+}
---
+++ 0x18-dynamic_libraries/1-strncat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strncat - function that concatenates two strings
+ * @dest: represent destination (first string)
+ * @src: represent the characters thats add them to destination
+ * @n: represent numbers of characters
+ * Return: dest as a pointer
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
