ac24bd5fe0ea35fa4e6296b48fbbf2c8ae878e95
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-07-06 09:20:37 -0700

    fix: task-0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,7 +8,7 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht = malloc(sizeof(hash_node_t) * size);
+	hash_table_t *ht = calloc(size, sizeof(hash_table_t));
 
 	if (size == 0)
 		return (NULL);
