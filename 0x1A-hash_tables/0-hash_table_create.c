cf9b201cfcae0dacda6bf65064008ad519491a34
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-07-06 01:44:03 -0700

    done: task-0 and task-1

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - function that creates a hash table.
+ * @size: hash table size
+ * Return: hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int index;
+
+	hash_table_t *ht = malloc(sizeof(hash_node_t) * size);
+
+	if (!ht)
+		return (NULL);
+
+	ht->size = size;
+	ht->array = (hash_node_t **) calloc(ht->size, sizeof(hash_node_t *));
+
+	for (index = 0; index < size; index++)
+		ht->array[index] = NULL;
+
+	return (ht);
+}
