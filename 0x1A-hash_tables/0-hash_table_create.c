9d0c002cafefb9bed402ee67e59db3f4e584078b
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-07-06 09:05:57 -0700

    fix: betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,6 +11,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int index;
 
 	hash_table_t *ht = malloc(sizeof(hash_node_t) * size);
+
 	if (!ht || size == 0)
 		return (NULL);
 
