6d5a70e345b87dd734328bc26ff267a99974b86e
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-07-06 09:39:59 -0700

    fix: task-4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,6 +12,10 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index = key_index((const unsigned char *)key, ht->size);
 	hash_node_t *current_node = ht->array[index];
 
+	if (!ht || !key || !*key)
+		return (NULL);
+
+
 	if (current_node != NULL)
 	{
 		if (strcmp(current_node->key, key) == 0)
