b8bb39f4dba8f90be60872f3403f4ff066a9b7d0
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-07-06 09:32:38 -0700

    fix: betty

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -13,7 +13,7 @@ void hash_table_print(const hash_table_t *ht)
 
 	if (ht == NULL)
 		return;
-	
+
 	printf("{");
 
 	for (; index < ht->size; index++)
@@ -31,3 +31,4 @@ void hash_table_print(const hash_table_t *ht)
 
 	printf("}\n");
 }
+
