704da14b8b573a97fed2802594093354aefd10fd
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-07-06 09:55:58 -0700

    fix: task-3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,10 +9,15 @@
 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_node = malloc(sizeof(hash_node_t));
+	hash_node_t *new_node;
 	unsigned long int index;
 
-	if (!new_node || !ht || strcmp(key, "") == 0)
+	if (!ht || !key || !*key)
+		return (0);
+
+	new_node = malloc(sizeof(hash_node_t));
+
+	if (!new_node)
 		return (0);
 
 	new_node->key = malloc(sizeof(char) * (strlen(key) + 1));
