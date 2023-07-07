5965d019e98420e78123dcc57bdab79c35f3f0d6
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-07-07 09:25:30 +0100

    task-3: fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,33 +1,6 @@
 #include "hash_tables.h"
 
 /**
- * add_node - adds a new node at the beginning of a linked list
- * @head: pointer to the list
- * @key: the key of the node
- * @value: node value
- * Return: the address of the new node, or NULL
- */
-
-hash_node_t *add_node(hash_node_t **head, const char *key, const char *value)
-{
-	hash_node_t *new_node;
-
-	new_node = malloc(sizeof(hash_node_t));
-
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
-	new_node->next = (*head);
-	(*head) = new_node;
-
-	return (*head);
-}
-
-
-
-/**
  * hash_table_set - function that adds an element to the hash table.
  * @ht: hash table
  * @key: node key
@@ -36,44 +9,43 @@ hash_node_t *add_node(hash_node_t **head, const char *key, const char *value)
 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_node;
-	unsigned long int index;
-	char *key_copy;
-	char *value_copy;
+	unsigned long int index = 0;
+	char *value_copy, *key_copy;
+	hash_node_t  *current_node, *new_node;
 
 	if (!ht || !key || !*key || !value)
 		return (0);
 
-	new_node = malloc(sizeof(hash_node_t));
-
-	if (!new_node)
+	value_copy = strdup(value);
+	if (!value_copy)
 		return (0);
 
-	new_node->key = malloc(sizeof(char) * (strlen(key) + 1));
-	new_node->value = malloc(sizeof(char) * (strlen(value) + 1));
+	index = key_index((const unsigned char *)key, ht->size);
+	current_node = ht->array[index];
 
-	if (new_node->key != NULL && new_node->value != NULL)
+	while (current_node != NULL)
 	{
-		key_copy = strdup(key);
-		value_copy = strdup(value);
-
-		new_node->key = key_copy;
-		new_node->value = value_copy;
+		if (!strcmp(key, current_node->key))
+		{
+			free(current_node->value);
+			current_node->value = value_copy;
+			return (1);
+		}
+		current_node = current_node->next;
 	}
-	else
+	new_node = calloc(1, sizeof(hash_node_t));
+	if (new_node == NULL)
 	{
-		free(key_copy);
 		free(value_copy);
 		return (0);
 	}
-
-	index = key_index((const unsigned char *)key, ht->size);
-
-	if (ht->array[index] != NULL)
-		ht->array[index] = new_node;
-	else
-		add_node(&(ht->array[index]), key, value);
-
+	key_copy = strdup(key);
+	if (!key_copy)
+		return (0);
+	new_node->key = key_copy;
+	new_node->value = value_copy;
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
 	return (1);
-}
 
+}
